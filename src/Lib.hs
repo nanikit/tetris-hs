83ba83a8333bfce28b2104b775d619559496c72d
nanikit <nanikit4@gmail.com>
2019-11-10 23:31:39 +0900

    Give up sdl2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -7,26 +8,42 @@ module Lib
   ) where
 
 import RIO
-import SDL
+import SDL as S
+import SDL.Font as F
 import Linear (V4(..))
+import Foreign.C.Types (CInt)
 
 runTetris :: IO ()
 runTetris = runSimpleApp tetris
 
 tetris :: RIO SimpleApp ()
 tetris = do
-  renderer <- getRenderer
-  appLoop renderer
+  context <- initDrawingContext  
+  runRIO context eventLoop
+  F.quit
+  S.quit
 
-getRenderer :: MonadIO m => m Renderer
-getRenderer = do
+data DrawingContext = DrawingContext
+  { window :: Window
+  , renderer :: Renderer
+  , font :: Font
+  }
+
+initDrawingContext :: MonadIO m => m DrawingContext
+initDrawingContext = do
   initializeAll
-  window <- createWindow "haskell tetris" defaultWindow
+  F.initialize
+  let config = defaultWindow
+        { windowInitialSize = V2 800 600
+        }
+  window <- createWindow "haskell tetris" config
   let driverIndex = -1
-  createRenderer window driverIndex defaultRenderer
+  renderer <- createRenderer window driverIndex defaultRenderer
+  font <- F.load "C:\\Windows\\Fonts\\malgun.ttf" 20
+  return (DrawingContext window renderer font)
 
-appLoop :: Renderer -> RIO s ()
-appLoop renderer = do
+eventLoop :: RIO DrawingContext ()
+eventLoop = do
   events :: Maybe Event <- waitEventTimeout 1000
   let eventIsQPress event =
         case eventPayload event of
@@ -35,7 +52,51 @@ appLoop renderer = do
             keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
           _ -> False
       qPressed = any eventIsQPress events
-  rendererDrawColor renderer $= V4 0 0 255 255
-  clear renderer
+
+  DrawingContext { window, renderer, font } <- ask
+
+  rendererDrawBlendMode renderer $= BlendAlphaBlend
+  fillBlackAll
+
+  let white = V4 255 255 255 255
+  rendererDrawColor renderer $= white
+
+  rendererRenderTarget renderer $= Nothing
+  -- screenTexture <- createTexture renderer RGBA8888 
+  scoreSurface <- solid font white "abcd"
+  scoreTexture <- createTextureFromSurface renderer scoreSurface
+
+  let rect l t w h = drawRect renderer (Just (ltwh l t w h))
+  rect 349 50 100 150
+
+  forM_ [0..9] $ \i -> do
+    let leftTop = (i + 0) * 10
+        side = 10
+    fillRect renderer (Just $ Rectangle (P (V2 leftTop leftTop)) (V2 side side))
+
+  -- copy renderer scoreTexture Nothing (Just (ltwh 450 100 100 30))
+  rect 50 50 300 500
+  copy renderer scoreTexture Nothing (Just (ltwh 450 200 100 30))
+
+
+  -- windowSurface <- getWindowSurface window
+  -- surfaceBlit scoreSurface Nothing windowSurface (Just (P (V2 470 100)))
+  -- updateWindowSurface window
+
+  freeSurface scoreSurface
+  destroyTexture scoreTexture
+
   present renderer
-  unless qPressed (appLoop renderer)
+  unless qPressed eventLoop
+
+ltwh :: CInt -> CInt -> CInt -> CInt -> Rectangle CInt
+ltwh left top width height = Rectangle leftTop widthHeight where
+  leftTop = P (V2 left top)
+  widthHeight = V2 width height
+
+fillBlackAll :: RIO DrawingContext ()
+fillBlackAll = do
+  DrawingContext { renderer } <- ask
+  rendererDrawColor renderer $= V4 0 0 0 255
+  clear renderer
+
